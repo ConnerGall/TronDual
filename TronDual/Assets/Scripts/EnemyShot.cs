5bd7632e796e081efa793b11809192f39c207051
PkSnoovie <166737867+PkSnoovie@users.noreply.github.com>
2026-03-18 12:22:41 -0400

    Enemy Shooty
    shot the player yay

--- TronDual/Assets/Scripts/EnemyShot.cs
+++ TronDual/Assets/Scripts/EnemyShot.cs
@@ -4,16 +4,18 @@ public class EnemyShot : MonoBehaviour
 {
     private Vector3 spawnPosition;
 
-    public float bulletLife = 1f;
+    public float bulletLife;
     //public float rotation = 0f;
-    public float speed = 1f;
+    public float speed;
     private float timer = 0f;
-    [SerializeField] GameObject player;
+    Camera mCamera;
     private Vector3 playerPosition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerPosition = new Vector3(player.transform.parent.position.x, player.transform.parent.position.y + 1, player.transform.parent.position.z);
+        mCamera = Camera.main;
+        playerPosition = new Vector3(mCamera.transform.position.x, mCamera.transform.position.y, mCamera.transform.position.z);
+        transform.Rotate(90.0f, 0f, 0f, Space.World);
     }
 
     // Update is called once per frame
@@ -28,13 +30,14 @@ public class EnemyShot : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, playerPosition, speed);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter3D(Collider collision)
     {
-        if (collision.CompareTag("PlayerHitBox"))
+        if (collision.CompareTag("MainCamera"))
         {
-            if(player != null)
+            if(mCamera != null)
             {
-                //DIE 
+                //dead
+                
             }
         }
     }
