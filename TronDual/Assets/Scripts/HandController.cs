4d84d3cffa3c061484a9eebd520bdbcc8306c0b6
ConnerGall <gallcs@miamioh.edu>
2026-03-30 20:53:48 -0400

    FinalHands

--- TronDual/Assets/Scripts/HandController.cs
+++ TronDual/Assets/Scripts/HandController.cs
@@ -4,21 +4,26 @@ using UnityEngine.InputSystem;
 public class HandController : MonoBehaviour
 {
     [SerializeField] private InputActionProperty gripAction;
+    [SerializeField] private InputActionProperty triggerAction;
     [SerializeField] private Hand hand;
 
     void OnEnable()
     {
         gripAction.action.Enable();
+        triggerAction.action.Enable();
     }
 
     void OnDisable()
     {
         gripAction.action.Disable();
+        triggerAction.action.Disable();
     }
 
     void Update()
     {
-        float closed = gripAction.action.ReadValue<float>();
+        float grip = gripAction.action.ReadValue<float>();
+        float trigger = triggerAction.action.ReadValue<float>();
+        float closed = Mathf.Max(grip, trigger);
         hand.SetClosed(closed);
     }
 }
